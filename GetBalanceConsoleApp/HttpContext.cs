791db156b63ea5b810f5b7ad74ecaa81f41aed9d
worklight <worklight>
2016-06-13 00:24:35 -0700

    Working proj before filtering requests

---
+++ GetBalanceConsoleApp/HttpContext.cs
@@ -0,0 +1,9 @@
+﻿namespace GetBalanceConsoleApp
+{
+    internal class HttpContext
+    {
+        public static HttpContext Current { get; internal set; }
+        public object Request { get; internal set; }
+        public object Response { get; internal set; }
+    }
+}
---
+++ GetBalanceConsoleApp/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.ServiceModel;
+using System.ServiceModel.Web;
+using System.ServiceModel.Description;
+
+namespace GetBalanceConsoleApp
+{ 
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Create the ServiceHost.
+            using (ServiceHost host = new ServiceHost(typeof(GetBalanceService)))
+            {
+                // Enable metadata publishing.
+                ServiceMetadataBehavior smb = new ServiceMetadataBehavior();
+                smb.HttpGetEnabled = true;
+
+                Console.WriteLine("The service is ready at {0}", host.BaseAddresses[0]);
+                host.Open();
+
+                Console.WriteLine("Press <Enter> to stop the service.");
+                Console.ReadLine();
+
+                // Close the ServiceHost.
+                host.Close();
+            }
+        }
+    }
+
+}
