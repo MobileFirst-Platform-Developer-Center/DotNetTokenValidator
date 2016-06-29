94417d3c66ef6ac8f58023e6e74f7de81d2e6ffd
Shmulik Bardosh <SHMULIKB@il.ibm.com>
2016-06-29 11:41:13 +0300

    MyExtensionBehavior class

---
+++ DotNetTokenValidator/Inspector/MyExtensionBehavior.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.ServiceModel.Channels;
+using System.ServiceModel.Configuration;
+using System.ServiceModel.Description;
+using System.ServiceModel.Dispatcher;
+
+namespace DotNetTokenValidator.Inspector
+{
+    public class MyExtensionBehavior : IEndpointBehavior
+    {
+        public void AddBindingParameters(ServiceEndpoint endpoint, BindingParameterCollection bindingParameters) { }
+        public void ApplyClientBehavior(ServiceEndpoint endpoint, ClientRuntime clientRuntime) { }
+ 
+        public void ApplyDispatchBehavior(ServiceEndpoint endpoint, EndpointDispatcher endpointDispatcher)
+        {
+            endpointDispatcher.DispatchRuntime.MessageInspectors.Add(new MyInspector());
+        }
+
+        public void Validate(ServiceEndpoint endpoint) { }
+    }
+
+    public class MyCustomBehaviorExtension : BehaviorExtensionElement
+    {
+        public override Type BehaviorType
+        {
+            get { return typeof(MyExtensionBehavior); }
+        }
+
+        protected override object CreateBehavior()
+        {
+            return new MyExtensionBehavior();
+        }
+    }
+}
